41b1a04e890faddc4f669b733a568c76165a338c
Кирилл Вязовов <kirviza92@yandex.ru>
2022-08-27 23:58:09 +0300

    asdfgh

--- backend/api_final/settings.py
+++ backend/api_final/settings.py
@@ -102,6 +102,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
